7ffb0c2b53e2f32c4ab283bdd99a8257cbd22135
Suman Khara <suman.khara@cerner.com>
2019-05-17 14:02:15 +0530

    added navbar

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -15,8 +15,9 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   </div>
   `,
   styles: [`
+    .thumbnail { min-height: 210px; }
     .pad-left { margin-left: 10px; }
-    .well div { color: red; }
+    .well div { color: #bbb; }
   `]
 })
 export class EventThumbnailComponent {
